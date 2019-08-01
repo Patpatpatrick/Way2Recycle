7b4002146631ba4490ebb8968ca6192c1cbdc109
Patpatpatrick <det8_opentitle@outlook.com>
2019-08-01 00:37:53 -0700

    map improvement

--- imports/ui/postProcedures/DetailUI/googleMap.jsx
+++ imports/ui/postProcedures/DetailUI/googleMap.jsx
@@ -10,15 +10,34 @@ Geocode.enableDebug();
 class MyComponents extends Component {
     constructor(props) {
         super(props);
-        this.map = React.createRef();
+        // this.map = React.createRef();
         this.state = {
             isMarkerShown:false,
+            center: {lat: 49.249606, lng: -123.112262}
         }
         this.addMarker = this.addMarker.bind(this);
+        this.handleMapLoad = this.handleMapLoad.bind(this);       
+        this.resetCenter = this.resetCenter.bind(this); 
+    }
+    componentDidUpdate(prevProps){
+        if(!this.state.isMarkerShown){
+            this.setState({
+                isMarkerShown: true,
+            })
+            // if(this.state.center.lat - this.props.item.location.lat < 49.03714345215196 - 49.05818322998725
+            //     || this.state.center.lat - this.props.item.location.lat > 49.05818322998725 - 49.03714345215196
+            //  ||  this.state.center.lng - this.props.item.location.lng > -122.42424320117186 + 122.48724292651366
+            //  || this.state.center.lng - this.props.item.location.lng < 122.42424320117186  -122.48724292651366 ){
+            //     this.setState({
+            //         center: this.props.item.location
+            //     })
+            //  }
+        }
     }
     addMarker(e) {
         const newPlace = { lat: e.latLng.lat(), lng: e.latLng.lng() };
         console.log(newPlace);
+        console.log(this.map);
         this.setState({
             isMarkerShown: true,
         })
@@ -34,6 +53,18 @@ class MyComponents extends Component {
             }
         );
     }
+    handleMapLoad(map){
+        console.log(map);
+        this.map = map;
+    }
+    resetCenter(){
+        let mapRef = this.map;
+        console.log(mapRef);
+        console.log(mapRef.getCenter().lat()+'; '+mapRef.getCenter().lng());
+        this.setState({
+            center: {lat: mapRef.getCenter().lat(), lng: mapRef.getCenter().lng()}
+        })
+    }
   render() {
      return (
             <GoogleMap
@@ -43,12 +74,13 @@ class MyComponents extends Component {
                     width: "800px",
                 }}
                 zoom={11}
-                center= {{lat: 49.249606, lng: -123.112262}}
+                center= {this.state.center}
                 onClick = {this.addMarker}
-                ref={map => {
-                    this.map = map;
-                }}
-                // onDragEnd={this._map.getCenter().toJSON()}
+                // ref={map => {
+                //     this.map = map;
+                // }}
+                onLoad = {this.handleMapLoad}
+                onDragEnd={this.resetCenter}
             >
                 {this.state.isMarkerShown &&
                 <Marker
