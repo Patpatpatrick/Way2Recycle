bc744397d7376c47801b08c05ad45ee1bd3d590e
Henry Lee <hlee2052@gmail.com>
2019-08-03 18:05:07 -0700

    forgot password change ui

--- imports/ui/SendPasswordToEmail.jsx
+++ imports/ui/SendPasswordToEmail.jsx
@@ -9,88 +9,119 @@ import Link from '@material-ui/core/Link';
 import Grid from '@material-ui/core/Grid';
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/core/styles';
+import {makeStyles} from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 import {connect} from "react-redux";
 
-import { updateFirstNameInputBox, updateLastNameInputBox, updateCreatePasswordInputPage,
-    updateCreateEmailInputPage, changeChoiceOnNav}  from '../actions/index.js';
+import {
+    updateFirstNameInputBox, updateLastNameInputBox, updateCreatePasswordInputPage,
+    updateCreateEmailInputPage, changeChoiceOnNav
+} from '../actions/index.js';
 
-class SendPasswordToEmail extends React.Component{
+class SendPasswordToEmail extends React.Component {
 
     constructor(props) {
         super(props)
-        this.state = {email: '', }
+        this.state = {
+            email: '',
+            loginSuccess: false,
+            pressedButton: false,
+        }
 
     }
 
     pressSendEmail = () => {
-        if (this.state.email ==='') {
-            alert("Please enter an email address")
+        if (this.state.email === '') {
+            /* alert("Please enter an email address")*/
+            this.setState({pressedButton: true, loginSuccess: false})
         } else {
-            Accounts.forgotPassword({email:this.state.email}, (err)=> {
-                alert('Please check your email')
+            Accounts.forgotPassword({email: this.state.email}, (err) => {
+                /*alert('Please check your email')*/
                 if (err) {
-                    alert("Problem with sending email address")
+                    this.setState({loginSuccess: false, pressedButton: true})
+                } else {
+                    this.setState({loginSuccess: true, pressedButton: true})
                 }
             })
         }
     }
 
-    changeEmail  = (event) =>  {
-        this.setState({email:event.target.value})
+    changeEmail = (event) => {
+        this.setState({email: event.target.value})
     };
 
     redirectToLogIn = () => {
         this.props.changeChoiceOnNav('login')
     }
 
-    render(){
-        return(
+    render() {
+        return (
             <Container component="main" maxWidth="xs">
-                <CssBaseline />
-                <div>
-                    <Avatar>
-                        <LockOutlinedIcon />
-                    </Avatar>
-                    <Typography component="h1" variant="h5">
-                    </Typography>
-                    <form noValidate>
-                        <div>Please enter the email address you used to register the account</div>
-                        <Grid container spacing={2}>
-                            <Grid item xs={12} sm={6}>
-                            </Grid>
-                            <Grid item xs={12}>
-                                <TextField
-                                    variant="outlined"
-                                    required
-                                    fullWidth
-                                    id="email"
-                                    label="Enter email"
-                                    autoComplete="email"
-                                    onChange={this.changeEmail}
-                                    type ="Email"
+                {/*  <CssBaseline />*/}
+                <div style={{paddingTop: 20}}>
+                    <div className={"logInBorder"}>
+                        <div>
 
-                                />
-                            </Grid>
-                            <Grid item xs={12}></Grid>
-                        </Grid>
-                        <Button
-                            fullWidth
-                            variant="contained"
-                            color="primary"
-                            onClick = {this.pressSendEmail}
-                        >
-                            Send Email
-                        </Button>
-                        <Grid container justify="flex-end">
-                            <Grid item>
-                                <Link variant="body2" onClick= {this.redirectToLogIn}>
-                                    Already have an account? Sign in
-                                </Link>
-                            </Grid>
-                        </Grid>
-                    </form>
+                            <form noValidate>
+                                <div>
+                                    <Typography component="h1" variant="h4">
+                                        Reset Password
+                                    </Typography>
+                                    <br/>
+                                    <Typography component="h1" variant="h5">
+                                        Please enter the email address you used to register the account
+                                    </Typography>
+                                </div>
+                                {
+                                    this.state.pressedButton && this.state.loginSuccess ?
+                                        <div>
+                                            <br/>
+                                            <Typography component="h1" variant="h4" color={"primary"}>Please check your
+                                                email!</Typography>
+                                        </div>
+                                        : (this.state.pressedButton && !this.state.loginSuccess) ?
+                                        <div>
+                                            <br/>
+                                            <Typography component="h1" variant="h5" color={"error"}>Please check that
+                                                your email is correct!</Typography>
+                                        </div> : null
+                                }
+                                <Grid container spacing={2}>
+                                    <Grid item xs={12} sm={6}>
+                                    </Grid>
+                                    <Grid item xs={12}>
+                                        <TextField
+                                            variant="outlined"
+                                            required
+                                            fullWidth
+                                            id="email"
+                                            label="Enter email"
+                                            autoComplete="email"
+                                            onChange={this.changeEmail}
+                                            type="Email"
+
+                                        />
+                                    </Grid>
+                                    <Grid item xs={12}></Grid>
+                                </Grid>
+                                <Button
+                                    fullWidth
+                                    variant="contained"
+                                    color="primary"
+                                    onClick={this.pressSendEmail}
+                                >
+                                    Send Email
+                                </Button>
+                                <Grid container justify="flex-end">
+                                    <Grid item>
+                                        <Link variant="body2" onClick={this.redirectToLogIn}>
+                                            Already have an account? Sign in
+                                        </Link>
+                                    </Grid>
+                                </Grid>
+                            </form>
+                        </div>
+                    </div>
                 </div>
             </Container>
         );
